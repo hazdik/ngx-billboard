7f0d10ce15d769d1c6504849968162e1d86c04df
Fozilbek <fozilbek.samiyev@gmail.com>
2017-07-22 13:11:05 +0500

    changed github pages src to docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp'),
   clean = require('del');
 
 gulp.task("default", () => {
-  clean('gh-pages/');
+  clean('docs/');
   gulp.task('default', function () {
     return gulp.src('dist/**')
-      .pipe(gulp.dest('gh-pages/'));
+      .pipe(gulp.dest('docs/'));
   });
 });
