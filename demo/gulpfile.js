f83b45f46b927bdeeadbb6755a22ddb5c33c028f
Fozilbek <fozilbek.samiyev@gmail.com>
2017-07-22 13:50:38 +0500

    fix root url erros

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
   clean = require('del');
 
 gulp.task("default", () => {
-  clean('../docs/');
+  clean('../docs/',  {force: true});
   gulp.task('default', function () {
     return gulp.src('dist/**')
       .pipe(gulp.dest('../docs/'));
