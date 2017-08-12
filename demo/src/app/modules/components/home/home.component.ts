db085cc87e5532f4dedbb2e7e48ca47f83d6caf4
Fozilbek <fozilbek.samiyev@gmail.com>
2017-08-12 21:39:18 +0500

    index file changes

--- demo/src/app/modules/components/home/home.component.ts
+++ demo/src/app/modules/components/home/home.component.ts
@@ -6,9 +6,42 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    private data: any;
+
     constructor() {
+        this.data = [
+            [
+                "data1",
+                30,
+                200,
+                100,
+                400,
+                150,
+                250
+            ],
+            [
+                "data2",
+                50,
+                20,
+                10,
+                40,
+                15,
+                25
+            ]
+        ]
     }
 
     ngOnInit() {
+        let data = [ ["x", "2013-01-11", "2013-01-21"],
+            ["data1", 500, 200],
+            ["data2", 100, 300],
+            ["data3", 200, 120]];
+        setInterval(()=>{
+            this.onUpdate(data)
+        }, 1500);
+    }
+
+    onUpdate(data){
+        // this.data = [...data];
     }
 }
--- src/modules/line-chart/components/line-chart.component.ts
+++ src/modules/line-chart/components/line-chart.component.ts
@@ -7,21 +7,57 @@ declare const bb: any;
     styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
-    @Input('data')
-    private data:any;
-
-    constructor() {
-    }
+    @Input('data') private data: any;
+    native: any;
 
     ngOnInit() {
-        var chart = bb.generate({
-            "data": {
-                "columns": [
-                    ["data1", 30, 200, 100, 400, 150, 250],
-                    ["data2", 50, 20, 10, 40, 15, 25]
-                ]
-            },
-            "bindto": "#LineChart"
+        this.native = bb.generate({
+            data: { columns: this.data && this.data.constructor === Array ? this.data : [] },
+            bindto: "#LineChart"
         });
+
+        this.native.categories([
+            "Category 1", "Category 2"
+        ]);
+
+        this.native.category(1, "Category 3");
+        let data = this.native.data();
+        setTimeout(() => {
+            this.native.focus("data1");
+            // this.native.regions([
+            //         { axis: "x", start: 5, class: "regionX" },
+            //         { axis: "y", end: 50, class: "regionY" }
+            //     ])
+            // this.native.resize({
+            //         width: 640,
+            //         height: 480
+            //     })
+            // this.native.select("data2", 1, true)
+            // this.native.xs({
+            //         data1: [10, 20, 30, 40,],
+            //         data2: [100, 200, 300, 400]
+            //     })
+            this.native.zoom([1, 4]);
+        }, 3000);
+
+    }
+
+    ngAfterViewInit() {
+
+    }
+
+    ngOnChanges(map: any) {
+        console.log(map, this.data);
+        if (map.data) this.onUpdateData();
+    }
+
+    onUpdateData() {
+        if (!this.data || this.data && this.data.constructor !== Array) throw new Error('Invalid data type.');
+        console.log(this.native);
+
+        if (this.native) {
+            this.native.load({ data: { columns: this.data }, });
+            this.native.unload({ ids: 'data1' });
+        }
     }
 }
